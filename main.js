b16591962e98169c68fb5a2d1a5b6c5d955e617a
André H Martello <andre.martello@mercadolivre.com>
2020-09-30 10:04:54 -0300

    Fix new url + allow search in RS monitor

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
 searchMonitor = function(selectedText){
+  searchWithURL(selectedText, "https://envios.adminml.com/network/monitor/")
+};
+
+searchRSMonitor = function(selectedText){
+  searchWithURL(selectedText, "https://envios.adminml.com/network/monitor-route-selector/")
+};
+
+searchWithURL = function(selectedText, monitorUrl){
   var rawValues = selectedText.selectionText;
 
   shippingIdRegex = /([0-9]{11})/;
 
   for (possibleShippingId of rawValues.split(shippingIdRegex)){
     if (shippingIdRegex.test(possibleShippingId)){
-      chrome.tabs.create({url: "https://shipping-bo.adminml.com/shipments/monitor/" + possibleShippingId});
+      chrome.tabs.create({url: monitorUrl + possibleShippingId});
     }
   }
 };
 
+searchBoth = function(selectedText){
+  searchMonitor(selectedText)
+  searchRSMonitor(selectedText)
+};
+
 chrome.contextMenus.create({
-  title: "Search in Monitor",
+  title: "SS Monitor",
   contexts:["selection"],
   onclick: searchMonitor
+});
+
+chrome.contextMenus.create({
+  title: "RS Monitor",
+  contexts:["selection"],
+  onclick: searchRSMonitor
+});
+
+chrome.contextMenus.create({
+  title: "RS and SS Monitor",
+  contexts:["selection"],
+  onclick: searchBoth
 });
